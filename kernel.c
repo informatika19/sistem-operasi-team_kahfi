2fd92f46da11162ce729cf2ba58000bc689b4da0
kahfizulkifli <68494704+kahfizulkifli@users.noreply.github.com>
2021-02-23 22:45:09 +0700

    Initial kit
    Kernel with function implemented
    TODO:
    - Startup boot with ASCII Art/Gambar
    - function int mod and div
    - makefile
    - Markdown

---
+++ kernel.c
@@ -0,0 +1,70 @@
+void handleInterrupt21 (int AX, int BX, int CX, int DX);
+void printString(char *string);
+void readString(char *string);
+void clear(char *buffer, int length); //Fungsi untuk mengisi buffer dengan 0
+
+int main() {
+  char *startup = "www";
+  printString(startup);
+  makeInterrupt21();
+  while (1);
+}
+
+void handleInterrupt21 (int AX, int BX, int CX, int DX){
+  switch (AX) {
+    case 0x0:
+      printString(BX);
+      break;
+    case 0x1:
+      readString(BX);
+      break;
+    default:
+      printString("Invalid interrupt");
+  }
+}
+
+void printString(char *string) {
+    int i = 0;
+    while (string[i] != '\0') {
+      putInMemory(0xB000,0x8000+i*2,string[i]);
+      putInMemory(0xB000,0x8000+((i*2)+1),0xD);
+      i++;
+    }
+}
+
+void readString(char *string) {
+  int count = 0;
+  int inputkar = 0;
+
+  while (1) {
+    // get char
+    inputkar = interrupt(0x16, 0, 0, 0, 0);
+    if (inputkar == 13) { //menerima clear
+      string[count] = 0; //null
+      string[count + 1] = 13; //clear
+      string[count + 2] = 10; //lf
+      interrupt(0x10, 0xe*256+13, 0, 0, 0);
+      interrupt(0x10, 0xe*256+10, 0, 0, 0);
+      return;
+    } else if (inputkar == 8) { //backspace
+      if (count > 0) {
+        string[count] = 0;
+        count--; //dihapus
+        interrupt(0x10, 0xe*256+8, 0, 0, 0); 
+        interrupt(0x10, 0xe*256+0, 0, 0, 0);
+        interrupt(0x10, 0xe*256+8, 0, 0, 0);
+      }
+    } else {
+      string[count] = inputkar; //menerima inputan
+      count++;
+      interrupt(0x10, 0xe*256+inputkar, 0, 0, 0);
+    }
+  }
+}
+
+void clear (char *buffer, int length) {
+  int i = 0;
+  for (i = 0; i < length; i++) {
+    buffer[i] = 0x0;
+  }
+}
