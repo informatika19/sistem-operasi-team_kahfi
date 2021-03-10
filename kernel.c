231f23d8fa0e9f3d40c5b1233a76c21611679ce3
kahfizulkifli <kahfizulkifli@students.itb.ac.id>
2021-03-11 02:24:14 +0700

    update structure

--- kernel.c
+++ kernel.c
@@ -2,6 +2,10 @@ void handleInterrupt21 (int AX, int BX, int CX, int DX);
 void printString(char *string);
 void readString(char *string);
 void clear(char *buffer, int length); //Fungsi untuk mengisi buffer dengan 0
+void readSector(char *buffer, int sector);
+void writeSector(char *buffer, int sector);
+void writeFile(char *buffer, char *path, int *sectors, char parentIndex);
+void readFile(char *buffer, char *path, int *result, char parentIndex);
 void logoHMIF();
 int div(int a, int b);
 int mod(int x, int y);
@@ -16,32 +20,48 @@ int main() {
   while (1);
 }
 
-void handleInterrupt21 (int AX, int BX, int CX, int DX){
-  switch (AX) {
-    case 0x0:
-      printString(BX);
-      break;
-    case 0x1:
-      readString(BX);
-      break;
-    default:
-      printString("Invalid interrupt");
-  }
+void handleInterrupt21 (int AX, int BX, int CX, int DX) { 
+  char AL, AH; 
+  AL = (char) (AX); 
+  AH = (char) (AX >> 8); 
+  switch (AL) { 
+    case 0x00: 
+      printString(BX); 
+      break; 
+    case 0x01: 
+      readString(BX); 
+      break; 
+    case 0x02: 
+      readSector(BX, CX); 
+      break; 
+    case 0x03: 
+      writeSector(BX, CX); 
+      break; 
+    /* case 0x04: 
+      readFile(BX, CX, DX, AH); 
+      break; 
+    case 0x05: 
+      writeFile(BX, CX, DX, AH); 
+      break; */
+    default: 
+      printString("Invalid interrupt"); 
+  } 
 }
+
 
 void printString(char *string) {
 char *ret = '\r';
 char *new = '\n';
 
-    while (*string != '\0') {
-        if ((*string) == ret) {
-            interrupt(0x10, 0xE00 + new, 0, 0, 0);
-        } else if ((*string) == new) {
-            interrupt(0x10, 0xE00 + ret, 0, 0, 0);
-        }
-        interrupt(0x10, 0xE00 + (*string), 0, 0, 0);
-        string++;
+  while (*string != '\0') {
+    if ((*string) == ret) {
+        interrupt(0x10, 0xE00 + new, 0, 0, 0);
+    } else if ((*string) == new) {
+        interrupt(0x10, 0xE00 + ret, 0, 0, 0);
     }
+    interrupt(0x10, 0xE00 + (*string), 0, 0, 0);
+    string++;
+  }
 }
 
 void readString(char *string) {
@@ -50,26 +70,26 @@ void readString(char *string) {
   char back = '\b';
 
   while (reading) {
-        char c = interrupt(0x16, 0, 0, 0, 0);
-        if (c == '\r') { // Return/Enter
-            interrupt(0x10, 0xE00 + '\r', 0, 0, 0);
-            interrupt(0x10, 0xE00 + '\n', 0, 0, 0);
-            (*string) = '\0';
-            reading = 0;
-    } else if (c == '\b') {
-            if (count > 0) {
-                interrupt(0x10, 0xE00 + '\b', 0, 0, 0);
-                interrupt(0x10, 0xE00 + '\0', 0, 0, 0);
-                interrupt(0x10, 0xE00 + '\b', 0, 0, 0);
-                (*string) = '\0';
-                string--;
-                count--;
-            }
-    } else {
-            interrupt(0x10, 0xE00 + c, 0, 0, 0);
-            (*string) = c;
-            string++;
-            count++;
+    char c = interrupt(0x16, 0, 0, 0, 0);
+    if (c == '\r') { // Return/Enter
+      interrupt(0x10, 0xE00 + '\r', 0, 0, 0);
+      interrupt(0x10, 0xE00 + '\n', 0, 0, 0);
+      (*string) = '\0';
+      reading = 0;
+} else if (c == '\b') {
+    if (count > 0) {
+      interrupt(0x10, 0xE00 + '\b', 0, 0, 0);
+      interrupt(0x10, 0xE00 + '\0', 0, 0, 0);
+      interrupt(0x10, 0xE00 + '\b', 0, 0, 0);
+      (*string) = '\0';
+      string--;
+      count--;
+    }
+} else {
+      interrupt(0x10, 0xE00 + c, 0, 0, 0);
+      (*string) = c;
+      string++;
+      count++;
     }
   }
 }
@@ -89,10 +109,20 @@ int div(int a, int b){
 }
 
 int mod(int x, int y) {
-    int i;
-    for (i=x; i>=y; i=i-y) {
-    }
-    return i;
+  int i;
+  for (i=x; i>=y; i=i-y) {
+  }
+  return i;
+}
+
+void readSector(char *buffer, int sector) {
+  interrupt(0x13, 0x201, buffer, div(sector, 36) * 0x100 + mod(sector, 18) 
+  + 1, mod(div(sector, 18), 2) * 0x100);
+}
+
+void writeSector(char *buffer, int sector) {
+  interrupt(0x03, 0x301, buffer, div(sector, 36) * 0x100 + mod(sector, 18) 
+  + 1, mod(div(sector, 18), 2) * 0x100);
 }
 
 void logoHMIF() {
